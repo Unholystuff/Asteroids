cda01eea6d0079ba868d75e2ab919d1c89b9ce1a
Unholystuff <joao.caixeta@hotmail.com>
2025-02-12 19:58:35 -0300

    FPS update

--- main.py
+++ main.py
@@ -2,19 +2,19 @@ import pygame
 from constants import *
 
 def main():
-    pygame.init
+    pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    print("Starting asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
+    clock = pygame.time.Clock()
+    dt = 0
 
-    running = True
-    while running:
+    while True:
+        for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    return
+                
         screen.fill("black")
         pygame.display.flip()
-
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            return
+        dt =  clock.tick(60)/ 1000
+        
 if __name__ == "__main__":
     main()
